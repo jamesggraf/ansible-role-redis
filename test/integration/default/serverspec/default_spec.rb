855f23ff3c15e95919ffae748a9fe541f7ed559c
Jim Graf <jim.graf@slalom.com>
2015-01-14 09:29:15 -0600

    Default

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -2,11 +2,13 @@ require 'spec_helper'
 describe port(6379) do
   it { should be_listening.on('0.0.0.0').with('tcp') }
 end
+ 
 if %w(debian ubuntu).include? os[:family]
   conffile = '/etc/redis/redis.conf'
 else
   conffile = '/etc/redis.conf'
 end
+ 
 describe file(conffile) do
   it { should be_file }
   its(:content) { should match /^bind 0\.0\.0\.0$/ }
